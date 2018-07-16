ec2db84d3681e22f16dc97ed6c1f59f89a28f3c1
Henadzi Miasnikou <henadzi.miasnikou@itechart-group.com>
2018-07-16 13:22:15 +0300

    Mobile App projects added

---
+++ GW2Assistant/Apps/GW2Assistant.Apps.MobileClient/GW2Assistant.Apps.MobileClient/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GW2Assistant.Apps.MobileClient.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ GW2Assistant/Apps/GW2Assistant.Apps.MobileClient/GW2Assistant.Apps.MobileClient/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GW2Assistant.Apps.MobileClient.Models;
+
+namespace GW2Assistant.Apps.MobileClient.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
